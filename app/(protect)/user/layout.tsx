7504f1c13ee44ef98b09b956e9423c44ff4f0658
sihyun-user <chsiyu8652@gmail.com>
2024-08-23 18:58:42 +0800

    style: 修改 UserLayout 樣式

--- app/(protect)/user/layout.tsx
+++ app/(protect)/user/layout.tsx
@@ -7,10 +7,10 @@ export default function UserLayout({
 }>) {
   return (
     <div className="min-page-screen m-auto flex w-full max-w-[1000px] flex-col rounded-lg bg-white md:flex-row">
-      <div className="mb-4 flex-1 border-r border-gray-200 p-6 md:mb-0">
+      <div className="mb-4 min-w-[280px] flex-none border-r border-gray-200 p-6 md:mb-0 md:flex-1">
         <UserMenu />
       </div>
-      <div className="w-full max-w-[648px]">{children}</div>
+      <div className="w-full max-w-full md:max-w-[648px]">{children}</div>
     </div>
   );
 }
